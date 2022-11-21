655daba060682cd78024982865bbb2badd13e4bf
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-21 21:59:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-function scuberGreetingForFeet(){
+const distance = 199;
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  
+
+  if (distance <=400){
+
+    return "This one is on me!";
+  }
+  
+  else if(distance  <= 2500){
+
+    return "I will gladly take your thirty bucks.";
+  }
+  
+  else if(distance > 2500){
+
+    return "No can do.";
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const city = "NYC"
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
+
   // Write your code here!
+
+ let checkCity = city? ('Ok, sounds good.') : ('No go.');
+
+ return checkCity;
+
+ 
+
+  
+  
+
+}
+const tip = "generous";
+
+let appreciation;
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+
+  switch (tip){
+
+    case "generous":
+      appreciation = "Thank you so much.";
+      break;
+      
+
+      case "not as generous":
+        appreciation = "Thank you.";
+        break;
+      
+
+       default:
+        appreciation = "Bye."
+          break;
+
+          
+
+  }
+  return appreciation;
 }
